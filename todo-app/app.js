67f99ec3135b5bb407c06a7c4b48d37d94bd7185
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-09-18 14:06:43 +0300

    1.5. The project, step 3

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,9 +1,13 @@
 import { createServer } from 'node:http';
+import fs from 'node:fs';
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT;
 
-const server = createServer();
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server started in port ${process.env.PORT}`);
